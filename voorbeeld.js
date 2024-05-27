66c97262b98d20c003539419a1dd746534ef9f81
BrightBoost <info@brightboost.nl>
2024-05-27 12:09:27 +0200

    if

--- voorbeeld.js
+++ voorbeeld.js
@@ -1,3 +1,22 @@
-function magic() {
-    document.getElementById("title").innerHTML = "Abrakadabra";
+function veranderNaam() {
+    let pageTitle = document.getElementById("title").innerHTML;
+    if(pageTitle == "Main heading") {
+        document.getElementById("title").innerHTML = "Abrakadabra";
+    } else {
+        document.getElementById("title").innerHTML = "Main heading";
+    }
 }
+
+let x = 2;
+let y = 2;
+
+if(x == y) {
+    console.log("x en y zijn gelijk");
+} else {
+    console.log("niet gelijk");
+}
+
+// maak een js bestand
+// link je html aan het js bestand
+// voeg toe alert("blabla")
+// open de pagina opnieuw
