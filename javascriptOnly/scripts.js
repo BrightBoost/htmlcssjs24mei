f4fcc5a4c5f3de0e7f0fd9dfe3f9a1c587702ea4
BrightBoost <info@brightboost.nl>
2024-06-03 09:28:17 +0200

    js

---
+++ javascriptOnly/scripts.js
@@ -0,0 +1,14 @@
+// variabelen
+let tekst = "Hoi Candy, Goeiemorgen!"; // dit is een tekst waarde, ook wel string genoemd
+let nummer = 11; // dit is een numerieke waarde
+let nummer2 = 12;
+
+let outputTekst = tekst + ", " + nummer;
+let totaal = nummer + nummer2;
+
+console.log(totaal); // string concatenation
+
+// operators
+
+
+// functies
